23e32a52dce07e9021e0de66460aab3a44facfa1
Filip Smets <alemaaltevinden@gmail.com>
2014-07-26 21:02:16 +0200

    Less annoying donate + forgot to add utils

---
+++ client/utils.js
@@ -0,0 +1,38 @@
+function activate (target) {
+    var buttons = document.getElementsByClassName("toolbutton");
+    for (var k = 0; k < buttons.length; k++) {
+        buttons[k].classList.remove("active");
+    }
+    target.classList.add("active");
+}
+
+function closeParent (target) {
+    if (target.parentNode) {
+        target.parentNode.style.display = 'none';
+    }
+}
+
+function donate () {
+    document.getElementById('donate').style.display = 'block';
+}
+
+function flashDonate () {
+    document.getElementById('donatebutton').classList.add('flash');
+    var times = 7;
+    function execFlash () {
+        if (times > 0) {
+            if (document.getElementById('donatebutton').classList.contains('flash')) {
+                document.getElementById('donatebutton').classList.remove('flash');
+            } else {
+                document.getElementById('donatebutton').classList.add('flash');
+            }
+            setTimeout(execFlash, 450);
+            times--;
+        } else {
+            if (document.getElementById('donatebutton').classList.contains('flash')) {
+                document.getElementById('donatebutton').classList.remove('flash');
+            }
+        }
+    }
+    setTimeout(execFlash, 450);
+}
